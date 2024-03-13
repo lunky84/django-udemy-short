33ee840827a633536a78e548840b584cea9d57ee
lunky84 <john.downie@thekeysupport.com>
2024-03-13 16:03:55 +0000

    add location model

--- django_course_site/meetups/admin.py
+++ django_course_site/meetups/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Meetup
+from .models import Meetup, Location
 
 # Register your models here.
 
@@ -11,3 +11,4 @@ class MeetupAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
 
 admin.site.register(Meetup, MeetupAdmin)
+admin.site.register(Location)

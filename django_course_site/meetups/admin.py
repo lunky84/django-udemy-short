e272eac9ce89acbaae088c79633b8ccc536818ba
lunky84 <john.downie@thekeysupport.com>
2024-03-13 11:44:31 +0000

    configure admin area

--- django_course_site/meetups/admin.py
+++ django_course_site/meetups/admin.py
@@ -4,4 +4,10 @@ from .models import Meetup
 
 # Register your models here.
 
-admin.site.register(Meetup)
+class MeetupAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'description')
+    list_filter = ('title', 'slug')
+    search_fields = ('title', 'slug')
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Meetup, MeetupAdmin)

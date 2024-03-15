81dcb27e64699275077f1609f13055a224cc0429
lunky84 <john.downie@thekeysupport.com>
2024-03-15 10:26:07 +0000

    passing organizer email

--- django_course_site/meetups/urls.py
+++ django_course_site/meetups/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('meetups/', views.index, name='all-meetups'),
-    path('meetups/registration-success', views.confirm_registration, name='confirm-registration'),
+    path('meetups/<slug:meetup_slug>/registration-success', views.confirm_registration, name='confirm-registration'),
     path('meetups/<slug:meetup_slug>', views.meetup_details, name='meetup-detail'),
 ]
